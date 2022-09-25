96e83579dcbae389918d0cc61844c8bb8bded99a
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-25 14:37:28 -0700

    add alt text to emoji

--- src/JokeList.js
+++ src/JokeList.js
@@ -45,6 +45,7 @@ class JokeList extends Component {
                     </h1>
                     <img 
                         src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
+                        alt="emoji"
                     />
                     <button className="JokeList-getmore">New Jokes</button>
                 </div>
