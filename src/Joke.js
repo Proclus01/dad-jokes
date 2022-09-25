777b16c0cbd95169b17ddfe10c1175a0f2816601
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-25 14:52:45 -0700

    create joke component boilerplate

---
+++ src/Joke.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Joke extends Component {
+    render() {
+        return (
+            <div className="Joke">
+                <div className="Joke-buttons">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Joke;
