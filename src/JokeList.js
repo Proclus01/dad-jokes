f3e8c8e3346f4a3e0f70907ab526371cf4d82113
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-25 17:13:13 -0700

    add functionality to load new jokes button, refactor setstate to save newly loaded state to local storage for persistence

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,10 +14,10 @@ class JokeList extends Component {
         this.state = {
             jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
         }
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-
         if (this.state.jokes.length === 0) {
             this.getJokes();
         }
@@ -35,7 +35,13 @@ class JokeList extends Component {
             jokes.push({id: uuidv4(), text: res.data.joke, votes: 0 });
         }
 
-        this.setState({ jokes: jokes });
+        this.setState(
+            st => ({
+                jokes: [...st.jokes, ...jokes]
+            }),
+            // Wait until state is updated and save the entire state to local storage
+            () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        );
 
         // Store jokes in our local storage
         window.localStorage.setItem(
@@ -53,8 +59,13 @@ class JokeList extends Component {
                 jokes: st.jokes.map(j => 
                     j.id === id ? {...j, votes: j.votes + delta} : j
                 )
-            })
+            }),
+            () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
         )
+    }
+
+    handleClick() {
+        this.getJokes();
     }
 
     render() {
@@ -84,7 +95,7 @@ class JokeList extends Component {
                         src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
                         alt="emoji"
                     />
-                    <button className="JokeList-getmore">New Jokes</button>
+                    <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 
                 <div className="JokeList-jokes">
